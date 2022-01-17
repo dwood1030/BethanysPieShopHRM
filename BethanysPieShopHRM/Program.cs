5b39610137b68d68d73d4d7aa94455e1c0bc3498
dwood1030 <dalton.woodard1@gmail.com>
2022-01-17 13:04:10 -0500

    created initial file with test variables

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BethanysPieShopHRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthlyWage = 1234;
+
+            Console.ReadLine();
+        }
+    }
+}
