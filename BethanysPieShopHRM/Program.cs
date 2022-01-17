482efdd6bf4be5acd6d2a200b17b17db2aa2d183
dwood1030 <dalton.woodard1@gmail.com>
2022-01-17 15:39:49 -0500

    added in time types and examples

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -39,7 +39,28 @@ namespace BethanysPieShopHRM
             char upperVersion = char.ToUpper(userSelection);
             bool isDigit = char.IsDigit(userSelection);
             bool isLetter = char.IsLetter(userSelection);
-            
+
+            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
+
+            Console.WriteLine(hireDate);
+
+            DateTime exitDate = new DateTime(2021, 12, 11);
+
+            // DateTime invalidDate = new DateTime(2021, 15, 11);
+
+            DateTime startDate = hireDate.AddDays(15);
+
+            Console.WriteLine(startDate);
+
+            DateTime currentDate = DateTime.Now;
+            bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+            DateTime startHour = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(8, 35, 0);
+            DateTime endHour = startHour.Add(workTime);
+
+            Console.WriteLine(startHour.ToLongDateString());
+            Console.WriteLine(endHour.ToShortTimeString());
 
             Console.ReadLine();
         }
