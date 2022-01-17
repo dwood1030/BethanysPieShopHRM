147c5ab8e7f3315fe7f79f03684ecc4fe5a8963a
dwood1030 <dalton.woodard1@gmail.com>
2022-01-17 13:20:01 -0500

    set up additional test variables to test that they are working correctly

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -7,6 +7,30 @@ namespace BethanysPieShopHRM
         static void Main(string[] args)
         {
             int monthlyWage = 1234;
+            int months = 12, bonus = 1000;
+
+            bool isActive = true;
+            double rating = 99.25;
+
+            double ratePerHour = 12.34;
+            int numberOfHoursWorked = 165;
+
+            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+
+            Console.WriteLine(currentMonthWage);
+
+            ratePerHour += 3; //ratePerHour = ratePerHour +3;
+
+            if(currentMonthWage > 2000)
+            {
+                Console.WriteLine("Top paid employee");
+            }
+
+            int numberOfEmployees = 15;
+            numberOfEmployees--;
+
+            bool a;
+            int b;
 
             Console.ReadLine();
         }
